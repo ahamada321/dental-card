ce422507a89366bce7b032f19af2d23abcbab945
Atsushi Hamada <a.hamada.biz@gmail.com>
2022-08-30 13:54:02 +0900

    Google Tag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
 import { HttpClientModule } from '@angular/common/http';
+import { GoogleTagManagerModule } from 'angular-google-tag-manager';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
@@ -8,7 +9,14 @@ import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, FooterComponent],
-  imports: [BrowserAnimationsModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    GoogleTagManagerModule.forRoot({
+      id: 'G-TJ7D8V74DY',
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
